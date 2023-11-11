a1df40cde49d8be64fa6cd2ca92b64f44968df99
eundeang <2021112027@dgu.ac.kr>
2023-11-11 19:52:52 +0900

    [setting] 프로젝트 기본 설정 완료 (mysql db연동, docker연동)

---
+++ EasyRouteAssistant/src/main/java/founders/EasyRouteAssistant/dto/UserRequestDTO.java
@@ -0,0 +1,15 @@
+package founders.easyRouteAssistant.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRequestDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+
+
+}
