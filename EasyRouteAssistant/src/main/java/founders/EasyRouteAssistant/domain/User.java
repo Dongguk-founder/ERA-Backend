6a9fa2a0aa3a694c6fb962cb4145b9fb1d87fd8c
eundeang <2021112027@dgu.ac.kr>
2023-11-11 20:09:22 +0900

    Revert "[setting] 프로젝트 기본 설정 완료 (mysql db연동, docker연동)"
    This reverts commit c5fb92fe7f2698077d675126df2a5bc79e7eba8d.

---
+++ EasyRouteAssistant/src/main/java/founders/EasyRouteAssistant/domain/User.java
@@ -0,0 +1,26 @@
+package founders.easyRouteAssistant.domain;
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+        @Id //primary key
+        private String id;
+        @Nonnull
+        private String name;
+        @Nonnull
+        private String email;
+        @Nonnull
+        private String password;
+        public User(String id, String name, String email,String password){
+            this.id = id;
+            this.name = name;
+            this.email = email;
+            this.password = password;
+        }
+
+}
+
