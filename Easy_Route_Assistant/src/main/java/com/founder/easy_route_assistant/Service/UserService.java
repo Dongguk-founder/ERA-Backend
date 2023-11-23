8251893362959797c451f412e3eeb17a36d56cd9
eundeang <2021112027@dgu.ac.kr>
2023-11-23 19:13:06 +0900

    [Fix] role 권한 부여 안되는 문제 해결 (default값 USER)

--- Easy_Route_Assistant/src/main/java/com/founder/easy_route_assistant/Service/UserService.java
+++ Easy_Route_Assistant/src/main/java/com/founder/easy_route_assistant/Service/UserService.java
@@ -4,6 +4,7 @@ import com.founder.easy_route_assistant.DTO.LoginDTO;
 import com.founder.easy_route_assistant.DTO.UserDTO;
 import com.founder.easy_route_assistant.Entity.UserEntity;
 import com.founder.easy_route_assistant.Repository.UserRepository;
+import com.founder.easy_route_assistant.security.Role;
 import com.founder.easy_route_assistant.token.JwtProvider;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -41,6 +42,7 @@ public class UserService {
                 .pwd(userDTO.getPwd())
                 .userName(userDTO.getUserName())
                 .userEmail(userDTO.getUserEmail())
+                .role(Role.USER)
                 .build();
         userEntity.setPwd(encodedPwd);
 
