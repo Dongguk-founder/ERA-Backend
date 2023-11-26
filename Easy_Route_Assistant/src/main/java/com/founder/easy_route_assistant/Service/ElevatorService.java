71ea35690ad9c99a175601c5a49a7b573fe5bb55
오수현 <elena0315@naver.com>
2023-11-26 21:58:38 +0900

    [Fix] api key 통일

--- Easy_Route_Assistant/src/main/java/com/founder/easy_route_assistant/Service/ElevatorService.java
+++ Easy_Route_Assistant/src/main/java/com/founder/easy_route_assistant/Service/ElevatorService.java
@@ -19,18 +19,18 @@ import java.util.regex.Pattern;
 @Service
 public class ElevatorService {
 
-    @Value("${ELEVATOR_URL}")
-    private String ELEVATOR_URL;
+    @Value("${SEOUL_URL}")
+    private String SEOUL_URL;
 
 
-    @Value("${ELEVATOR_APPKEY}")
-    private String ELEVATOR_APPKEY;
+    @Value("${SEOUL_KEY}")
+    private String SEOUL_KEY;
 
 
     public List<ElevatorDTO> requestElevatorAPI(String sw_cd) {
 
         //UriBulider 설정을 해주는 DefaultUriBuilderFactory
-        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(ELEVATOR_URL);
+        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(SEOUL_URL);
 
         //API를 사용하기 위한 인증key를 사용하기 위해 인코딩을 해야함 (그냥 호출하게 되면 API키가 달라지는 경우가 생길 수 있다.)
         //VALUES_ONLY : URI템플릿은 인코딩하지 않고 URI변수를 템플릿에 적용하기 전에 엄격히 인코딩한다.
@@ -40,14 +40,14 @@ public class ElevatorService {
         //WebClient 디폴트 세팅
         WebClient webClient = WebClient.builder()
                 .uriBuilderFactory(factory)
-                .baseUrl(ELEVATOR_URL)
+                .baseUrl(SEOUL_URL)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
 
         //WebClient.get() Returns: a spec for specifying the target URL
         String getstring = webClient.get()
                 .uri(uriBuilder -> uriBuilder
-                        .path(ELEVATOR_APPKEY)
+                        .path(SEOUL_KEY)
                         .path("/json/tbTraficElvtr/1/500/")
                         .build())
                 .retrieve()
