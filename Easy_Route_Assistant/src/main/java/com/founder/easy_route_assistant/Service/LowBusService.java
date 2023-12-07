a93b9e7288f78cae2ecc04f97a8b27f471c0f2ec
eundeang <111877048+eundeang@users.noreply.github.com>
2023-12-07 20:17:10 +0900

    [Feat] 필터링 확인하기

--- Easy_Route_Assistant/src/main/java/com/founder/easy_route_assistant/Service/LowBusService.java
+++ Easy_Route_Assistant/src/main/java/com/founder/easy_route_assistant/Service/LowBusService.java
@@ -1,13 +1,23 @@
 package com.founder.easy_route_assistant.Service;
 
+import com.founder.easy_route_assistant.Entity.LowBusEntity;
+import com.founder.easy_route_assistant.Repository.LowBusRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class LowBusService {
-    public void filteringRoute (String busNum){
-
+    private final LowBusRepository lowBusRepository;
+    public String filteringRoute (String busNum){
+        Optional<LowBusEntity> busEntity = lowBusRepository.findById(busNum);
+        if (busEntity.isPresent()){
+            return busEntity.get().getRouteid();
+        } else {
+            return null;
+        }
     }
 
 }
